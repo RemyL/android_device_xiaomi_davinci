a392c1a06a668bfcc57e2c00db9f498a89c7ff58
RemyL <remy.lampure@free.fr>
2021-06-08 14:16:32 +0200

    davinci: Rename lunch choices from lineage to arrow

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -8,6 +8,6 @@ PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/arrow_davinci.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_davinci-user \
-    lineage_davinci-userdebug \
-    lineage_davinci-eng
+    arrow_davinci-user \
+    arrow_davinci-userdebug \
+    arrow_davinci-eng
